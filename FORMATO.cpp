fb00e11f855a36feccf767e30fc6708dc0621f92
Gabi-main <147835595+Gabi-main@users.noreply.github.com>
2024-10-01 16:21:58 -0500

    Add files via upload

---
+++ FORMATO.cpp
@@ -0,0 +1,9 @@
+﻿//FORMATO PARA LA CREACION DEL OBJETO
+
+Elemento hidrogeno("Hidrogeno", "1s1", 1, 0, 0, 1);
+
+
+//FORMATO PARA EL PROTOCOLO DEL OBJETO
+
+} else if (n == helio.n && l == helio.l && ml == helio.ml && ms == helio.ms) {
+				helio.imprimirDatos();
