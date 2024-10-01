0f1a721a698f52546f9268784b48ed5e5a903a1d
Gabi-main <147835595+Gabi-main@users.noreply.github.com>
2024-10-01 17:06:19 -0500

    Update FORMATO.cpp

--- FORMATO.cpp
+++ FORMATO.cpp
@@ -3,7 +3,3 @@
 Elemento hidrogeno("Hidrogeno", "1s1", 1, 0, 0, 1);
 
 
-//FORMATO PARA EL PROTOCOLO DEL OBJETO
-
-} else if (n == helio.n && l == helio.l && ml == helio.ml && ms == helio.ms) {
-				helio.imprimirDatos();
