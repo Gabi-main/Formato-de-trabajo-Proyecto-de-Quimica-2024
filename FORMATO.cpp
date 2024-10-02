785acde0b67969b6d427944e39d12c4305e14bcf
Gabi-main <147835595+Gabi-main@users.noreply.github.com>
2024-10-02 11:41:39 -0500

    Update FORMATO.cpp

--- FORMATO.cpp
+++ FORMATO.cpp
@@ -1,5 +1,11 @@
-﻿//FORMATO PARA LA CREACION DEL OBJETO
+//Formato para el desarrollo de los elementos segun sus valores cuanticos
+// Cada valor representa , en orden, nombre, configuracion electronica, numero n, numero l, numero ml y numero ms
+elemento.push_back(elementocuantico("oxígeno" ,"2p4",2, 1, 0, -1));
 
-Elemento hidrogeno("Hidrogeno", "1s1", 1, 0, 0, 1);
 
 
+//Formato para el desarrollo de los elementos segun la tabla
+//Cada valor dentro del parentesis respresenta, en orden, nombre, numero atomico, masa atomica, radio atomico
+// protones, neutrones y electrones en el elemento
+
+elementos.push_back(atomico("hidrógeno", 1, 1.008, 53, 1, 0, 1));
